029b0f6ab7391af51692648b3e98061066422d55
DESKTOP-2T5T6AG\SSS <963292639@qq.com>
2018-05-19 08:05:38 +0800

    no message

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,56 +1,65 @@
 <template>
-  <div class="shopcart">
-    <div class="content" @click="toggleList">
-      <div class="content-left">
-        <div class="logo-wrapper">
-          <div class="logo" :class="{'hightlight':totalCount>0}">
-            <span class="icon-shopping_cart" :class="{'hightlight':totalCount>0}"></span>
+  <div>
+    <div class="shopcart">
+      <div class="content" @click="toggleList">
+        <div class="content-left">
+          <div class="logo-wrapper">
+            <div class="logo" :class="{'hightlight':totalCount>0}">
+              <span class="icon-shopping_cart" :class="{'hightlight':totalCount>0}"></span>
+            </div>
+            <div v-show="totalCount>0" class="num">{{totalCount}}</div>
           </div>
-          <div v-show="totalCount>0" class="num">{{totalCount}}</div>
+          <div class="price" :class="{'hightlight':totalPrice>0}">¥{{totalPrice}}元</div>
+          <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
         </div>
-        <div class="price" :class="{'hightlight':totalPrice>0}">¥{{totalPrice}}元</div>
-        <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
+        <div class="content-right">
+          <div class="pay" @click.stop="pay" :class="payClass">{{payDesc}}</div>
+        </div>
       </div>
-      <div class="content-right">
-        <div class="pay" :class="payClass">{{payDesc}}</div>
+      <div class="ball-container">
+        <transition name="ball"
+                    v-for="(ball, index) in balls"
+                    :key="index"
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter">
+          <div class="ball" v-show="ball.show">
+            <div class="inner inner-hook"></div>
+          </div>
+        </transition>
       </div>
-    </div>
-    <div class="ball-container">
-      <transition name="fade"
-                  v-for="(ball, index) in balls"
-                  :key="index"
-                  @before-enter="beforeEnter"
-                  @enter="enter"
-                  @after-enter="afterEnter">
-        <div class="ball" v-show="ball.show">
-          <div class="inner inner-hook"></div>
+      <transition name="list-fede">
+        <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty" @click="empty">清空</span>
+          </div>
+          <div class="list-content" ref="listContent">
+            <ul>
+              <li class="food" v-for="food in selectFoods">
+                <span class="name">{{food.name}}</span>
+                <div class="price">
+                  <span>¥{{food.price*food.count}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </transition>
     </div>
-    <div class="shopcart-list" v-show="listShow">
-      <div class="list-header">
-        <h1 class="title">购物车</h1>
-        <span class="empty">清空</span>
-      </div>
-      <div class="list-content">
-        <ul>
-          <li class="food" v-for="food in selectFoods">
-            <span class="name">{{food.name}}</span>
-            <div class="price">
-              <span>¥{{food.price*food.count}}</span>
-            </div>
-            <div class="cartcontrol-wrapper">
-              <cartcontrol :food="food"></cartcontrol>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <transition name="mask">
+      <div class="show-mask" v-show="listShow" @click="toggleList"></div>
+    </transition>
   </div>
+
 </template>
 
 <script>
     import Cartcontrol from "../cartcontrol/cartcontrol";
+    import BScroll from "better-scroll"
 
     export default {
       components: {Cartcontrol},
@@ -137,10 +146,32 @@
             return false
           }
           let show = !this.fold;
+          if (show) {
+            this._initScroll()
+          }
           return show;
         }
       },
       methods: {
+        _initScroll() {
+          this.$nextTick(()=>{
+            if (!this.scroll) {
+              this.scroll = new BScroll(this.$refs.listContent, {
+                click: true
+              })
+            } else {
+              this.scroll.refresh()
+            }
+          })
+        },
+        pay () {
+
+        },
+        empty() {
+          this.selectFoods.forEach((food)=>{
+            food.count = 0
+          })
+        },
         toggleList() {
           if (!this.totalCount) {
             return
@@ -200,6 +231,9 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+
+  @import "../../common/stylus/mixin.styl"
+
   .shopcart
     position fixed
     left 0
@@ -298,12 +332,16 @@
           border-radius 50%
           background rgb(0,160,220)
           transition: all .6s linear
+    .list-fede-enter-active, .list-fede-leave-active
+      transition: all 0.4s
+    .list-fede-enter, .list-fede-leave-to
+      transform: translate3d(0, 100%, 0)
     .shopcart-list
       position absolute
       left 0
-      top 0
-      z-index -1
+      bottom 48px
       width 100%
+      z-index -1
       .list-header
         height 40px
         line-height 40px
@@ -323,5 +361,40 @@
         max-height 217px
         overflow hidden
         background #fff
+        .food
+          position relative
+          padding 12px 0
+          box-sizing border-box
+          border-1px(rgba(7,17,27,0.1))
+          .name
+            line-height 24px
+            font-size 14px
+            color rgb(7,17,27)
+          .price
+            position absolute
+            right 100px
+            bottom 12px
+            line-height 24px
+            font-size 14px
+            font-weight 700
+            color rgb(240,20,20)
+          .cartcontrol-wrapper
+            position absolute
+            right 0
+            bottom 6px
 
+
+  .mask-enter-active, .mask-leave-active
+    transition opacity .5s;
+  .mask-enter, .mask-leave-to
+    opacity 0
+  .show-mask
+    position fixed
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index 40
+    -webkit-backdrop-filter blur(10px)
+    background rgba(7,17,27,0.6)
 </style>
